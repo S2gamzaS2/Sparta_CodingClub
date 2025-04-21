6b7374ab03c343b0bca292e1162b2e069c9dfac1
Sohae Park <ipnsohe@naver.com>
2025-04-22 00:31:55 +0900

    에러 발생 시 메세지를 err.product 큐로 전송

--- RabbitMQ/sagaPattern/payment/src/main/java/com/market/payment/PaymentService.java
+++ RabbitMQ/sagaPattern/payment/src/main/java/com/market/payment/PaymentService.java
@@ -3,6 +3,8 @@ package com.market.payment;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -10,8 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PaymentService {
 
-  public void createPayment(DeliveryMessage deliveryMessage) {
+  private final RabbitTemplate rabbitTemplate;
 
+  @Value("${message.queue.err.product}")
+  private String productErrQueue;
+
+  public void createPayment(DeliveryMessage deliveryMessage) {
 
     Payment payment = Payment.builder()
         .paymentId(UUID.randomUUID())
@@ -23,9 +29,19 @@ public class PaymentService {
     // 결제 금액이 10000원 이상이면 에러
     if(deliveryMessage.getPayAmount() >= 10000) {
       log.error("#### Payment Amount Exceeds Limit: {}", payment.getPayAmount());
+      deliveryMessage.setErrorType("PAYMENT_LIMIT_EXCEEDED");
       payment.setPayStatus("FAILED");
-      return;
+
+      this.rollbackPayment(deliveryMessage, payment.getPaymentId());
     }
+  }
+
+
+  // RabbitTemplate을 통해 에러 큐로 보내는 함수
+  public void rollbackPayment(DeliveryMessage deliveryMessage, UUID paymentId) {
+    log.info("***** PAYMENT ROLLBACK!!!!");
+    deliveryMessage.setPaymentId(paymentId);
+    rabbitTemplate.convertAndSend(productErrQueue, deliveryMessage);
   }
 
 }
