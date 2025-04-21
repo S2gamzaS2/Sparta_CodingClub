566d35e54fc7dd2e0592b8550a84549520a9b315
Sohae Park <ipnsohe@naver.com>
2025-04-21 23:58:23 +0900

    주문 생성 시 RabbitMQ를 통해 DeliveryMessage를 Product로 전송하는 로직 추가

--- RabbitMQ/sagaPattern/order/src/main/java/com/market/order/OrderService.java
+++ RabbitMQ/sagaPattern/order/src/main/java/com/market/order/OrderService.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,15 +14,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
 
+  private final RabbitTemplate rabbitTemplate;
+
+  @Value("${message.queue.product}")
+  private String productQueue;
+
+
   private Map<UUID, Order> orderStore = new HashMap<>();
 
 
   public Order createOrder(OrderEndpoint.OrderRequestDto dto) {
     Order order = dto.toOrder();
-
     orderStore.put(order.getOrderId(), order);
+
+    DeliveryMessage deliveryMessage = dto.toDeliveryMessage(order.getOrderId());
+
+    // RabbitMQ
+    rabbitTemplate.convertAndSend(productQueue, deliveryMessage);
+
     return order;
   }
+
 
   public Order getOrder(UUID orderId) {
     return orderStore.get(orderId);
